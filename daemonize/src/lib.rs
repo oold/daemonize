e19a4ac566a81758d6e9d9d721494c3c7beea563
Oliver Old <mail@oold.dev>
2025-04-04 18:30:36 +0200

    Update license, author, and repo info

--- daemonize/src/lib.rs
+++ daemonize/src/lib.rs
@@ -1,10 +1,21 @@
-// Copyright (c) 2016 Fedor Gogolev <knsd@knsd.net>
+// SPDX-License-Identifier: MIT OR Apache-2.0
 //
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
+// This file is licensed under the MIT license or the Apache License, Version 2.0, at your choice.
+//
+// Copyright 2016 Fedor Gogolev
+// Copyright 2025 Oliver Old
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 //!
 //! daemonize is a library for writing system daemons. Inspired by the Python library [thesharp/daemonize](https://github.com/thesharp/daemonize).
